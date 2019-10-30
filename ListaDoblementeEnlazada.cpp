ce75cd0771e5141ca3a96b8d41f6d4d4edf88ae8
DavidVallecios <37277365+DavidVallecios@users.noreply.github.com>
2019-10-30 15:39:12 -0600

    Update ListaDoblementeEnlazada.cpp

--- ListaDoblementeEnlazada.cpp
+++ ListaDoblementeEnlazada.cpp
@@ -4,3 +4,39 @@ struct Nodo{
   Nodo *prev;
 }*inicio, *fin;
 
+class ListaDobleEnlazada{
+
+  public:
+    Nodo* crearNodo(int valor){
+      Nodo *n = new Nodo;
+      n->dato = valor;
+      n->sig = nullptr;
+      n->prev = nullptr;
+      return n;
+    }
+    
+    void mostrarListaAdelante(){
+      Nodo *temp = inicio;
+      if(!inicio)
+        cout << "La lista no posee elementos" << endl;
+      else
+        while(temp){
+          cout << temp->dato<<", ";
+          temp = temp->sig;
+        }
+     }
+     
+     void agregarElemento(int valor){
+       Nodo *n = crearNodo(valor), *temp;
+       if(!inicio)
+        inicio = n;
+       else{
+        for(temp=inicio; temp->sig; temp = temp->sig);
+          temp->sig = n;
+       }
+      }
+      
+      ListaEnlazada(){
+        inicio=nullptr;
+       }
+};
