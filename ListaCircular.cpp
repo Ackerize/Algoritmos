3cadc7eb9d6c574e7a6674e43f33ce33c55b0284
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2019-11-20 19:00:00 -0600

    Update ListaCircular.cpp

--- ListaCircular.cpp
+++ ListaCircular.cpp
@@ -1,1 +1,47 @@
+struct Nodo{
+  int dato;
+  Nodo *sig;
+  Nodo *prev;
+}*inicio, *fin;
 
+
+class ListaCircular{
+
+  public:
+    Nodo* crearNodo(int valor){
+      Nodo *n = new Nodo;
+      n->dato = valor;
+      n->sig = nullptr;
+      return n;
+    }
+    
+    void mostrarLista(){
+        Nodo *temp = fin->sig;
+        if(!fin)
+            cout << "La lista no posee elementos" << endl;
+        while(temp != fin){
+            cout << temp->dato<<", ";
+            temp = temp->sig;
+        }
+        cout << temp->dato;
+    }
+        
+    void agregarElemento(int valor){
+        Nodo *n = crearNodo(valor);
+        if(!fin){
+            fin = n;
+            n->sig = fin;
+        }
+        else{
+            n->sig = fin->sig;
+            fin->sig = n;
+            fin = n;
+        }
+
+    }
+        
+    ListaCircular(){
+        inicio=nullptr;
+        fin = nullptr;
+    }
+};
